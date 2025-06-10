987d628bb24f57b11ae096980e950750d1967df7
06israt <israt.jahan.hira@g.bracu.ac.bd>
2025-06-10 22:15:32 +0600

    cart-bottom

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext,  useState } from "react";
 import { food_list } from "../assets/assets.js";
 
 export const StoreContext = createContext(null);
@@ -18,9 +18,17 @@ const StoreContextProvider = (props) => {
     setCartsItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item)
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+
+    }
+    return totalAmount;
+  }
 
   const contextValue = {
     food_list,
@@ -28,6 +36,7 @@ const StoreContextProvider = (props) => {
     setCartsItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount,
   };
   return (
     <StoreContext.Provider value={contextValue}>
